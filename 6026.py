b9c4b9bb865583fc3bdd6aff29250e6566d3009d
99millar <wlwogus33@gmail.com>
2022-05-11 21:32:12 +0900

    6026~6037

---
+++ 6026.py
@@ -0,0 +1,8 @@
+n1 = input()
+n2 = input()
+n1 = float(n1)
+n2 = float(n2)
+
+sum = n1 + n2
+
+print(sum)
---
+++ 6036.py
@@ -0,0 +1,5 @@
+word, num = input().split()
+print(word*int(num))
+
+# 참고
+# 문자열 * 정수 또는 정수 * 문자열은 그 문자열을 여러 번 반복한 문자열을 만들어 준다.
