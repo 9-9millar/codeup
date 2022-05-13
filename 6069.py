0af629ffb2f8fd5611a36574c5a4bff79157ef02
99millar <wlwogus33@gmail.com>
2022-05-13 15:01:34 +0900

    6068~6076

---
+++ 6069.py
@@ -0,0 +1,11 @@
+rate_message = input()
+if rate_message == 'A':
+    print('best!!!')
+elif rate_message == 'B':
+    print('good!!')
+elif rate_message == 'C':
+    print('run!')
+elif rate_message == 'D':
+    print('slowly~')
+else:
+    print('what?')
